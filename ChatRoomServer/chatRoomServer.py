6063358ddbbcdd19f08b6d4edc82997937e8acc1
Yunus Emre Karaköse <yemre05@hotmail.com.tr>
2019-11-19 23:45:20 +0300

    crs düzenleme

--- ChatRoomServer/chatRoomServer.py
+++ ChatRoomServer/chatRoomServer.py
@@ -6,135 +6,120 @@ HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 1234
 
-# Create a socket
-# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
-# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+# Create a socket TCP/IPv4
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# SO_ - socket option
-# SOL_ - socket option level
-# Sets REUSEADDR (as a socket option) to 1 on socket
+# Set REUSEADDR (adresin tekrar kullanılabilmesi için)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# Bind, so server informs operating system that it's going to use given IP and port
-# For a server using 0.0.0.0 means to listen on all available interfaces, useful to connect locally to 127.0.0.1 and remotely to LAN interface IP
-server_socket.bind((IP, PORT))
-
-# This makes server listen to new connections
+#server
+server_address = (IP, PORT)
+# Bind server with ıp/port
+server_socket.bind(server_address)
+# Yeni bağlantıları dinle
 server_socket.listen()
 
-# List of sockets for select.select()
+# Soket Listesi
 sockets_list = [server_socket]
 
-# List of connected clients - socket as a key, user header and name as data
+# Bağlı Soket Listesi
 clients = {}
 
 print(f'Listening for connections on {IP}:{PORT}...')
 
-# Handles message receiving
+# Mesaj alma fonksiyonu
 def receive_message(client_socket):
 
     try:
 
-        # Receive our "header" containing message length, it's size is defined and constant
+        # Mesajı Al 
         message_header = client_socket.recv(HEADER_LENGTH)
 
-        # If we received no data, client gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+        # Mesaj yoksa
         if not len(message_header):
             return False
 
-        # Convert header to int value
+        # Mesaj uzunluğu
         message_length = int(message_header.decode('utf-8').strip())
 
-        # Return an object of message header and message data
+        # Mesajı obje olarak döndür
         return {'header': message_header, 'data': client_socket.recv(message_length)}
 
     except:
-
-        # If we are here, client closed connection violently, for example by pressing ctrl+c on his script
-        # or just lost his connection
-        # socket.close() also invokes socket.shutdown(socket.SHUT_RDWR) what sends information about closing the socket (shutdown read/write)
-        # and that's also a cause when we receive an empty message
+        # Diğer durumlar için false döndür 
         return False
 
 while True:
 
-    # Calls Unix select() system call or Windows select() WinSock call with three parameters:
-    #   - rlist - sockets to be monitored for incoming data
-    #   - wlist - sockets for data to be send to (checks if for example buffers are not full and socket is ready to send some data)
-    #   - xlist - sockets to be monitored for exceptions (we want to monitor all sockets for errors, so we can use rlist)
+    #  Unix select() yada Windows select() WinSock 3 parametre ile çağır:
+    #   - rlist - gelen mesajın izleneceği soketler
+    #   - wlist - mesaj gönderilecek soketler
+    #   - xlist - hata veren soketler
     # Returns lists:
-    #   - reading - sockets we received some data on (that way we don't have to check sockets manually)
-    #   - writing - sockets ready for data to be send thru them
-    #   - errors  - sockets with some exceptions
-    # This is a blocking call, code execution will "wait" here and "get" notified in case any action should be taken
+    #   - reading - mesaj aldığımız soketler
+    #   - writing - mesaj almaya uygun soketler
+    #   - errors  - diğer durumdaki soketler
     read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
 
-    # Iterate over notified sockets
+    # Soket Listesindeki her soket için
     for notified_socket in read_sockets:
 
-        # If notified socket is a server socket - new connection, accept it
+        # Yeni bağlantı kabul et
         if notified_socket == server_socket:
 
-            # Accept new connection
-            # That gives us new socket - client socket, connected to this given client only, it's unique for that client
-            # The other returned object is ip/port set
+            # Kabul edilen bağlantı soketi ve adresi
             client_socket, client_address = server_socket.accept()
 
-            # Client should send his name right away, receive it
+            # Client ismini al
             user = receive_message(client_socket)
 
-            # If False - client disconnected before he sent his name
+            # İsim yok is client isim yollamadan kapanmıştır
             if user is False:
                 continue
 
-            # Add accepted socket to select.select() list
+            # Kabul edilen bağlantıyı  select.select() listesine ekle
             sockets_list.append(client_socket)
 
-            # Also save username and username header
+            # Client ismi ve başlığı
             clients[client_socket] = user
 
             print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
 
-        # Else existing socket is sending a message
+        # Var olan soket 
         else:
 
-            # Receive message
+            # Mesaj al
             message = receive_message(notified_socket)
 
-            # If False, client disconnected, cleanup
+            # Mesaj yoksa client kapandı
             if message is False:
                 print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
 
-                # Remove from list for socket.socket()
+                #  socket.socket() listesinden kaldır
                 sockets_list.remove(notified_socket)
 
-                # Remove from our list of users
+                # Client listesinden kaldır
                 del clients[notified_socket]
 
                 continue
 
-            # Get user by notified socket, so we will know who sent the message
+            # Mesajı kimin gönderdiğini kontrol et
             user = clients[notified_socket]
 
             print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
 
-            # Iterate over connected clients and broadcast message
+            # Diğer clientlara alınan mesajı  yolla
             for client_socket in clients:
 
-                # But don't sent it to sender
+                # Kendi Kendine gönderme
                 if client_socket != notified_socket:
-
-                    # Send user and message (both with their headers)
-                    # We are reusing here message header sent by sender, and saved username header send by user when he connected
                     client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
 
-    # It's not really necessary to have this, but will handle some socket exceptions just in case
+    # Hatalı(exepction) soketleri kaldır
     for notified_socket in exception_sockets:
 
-        # Remove from list for socket.socket()
+        # socket.socket() listesinden kaldır
         sockets_list.remove(notified_socket)
 
-        # Remove from our list of users
+        # clientlardna kaldır
         del clients[notified_socket]
+        
