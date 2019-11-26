1ff45c8ad1ac51a1de09df55decb3ea02e5eecd9
Yunus Emre Karaköse <yemre05@hotmail.com.tr>
2019-11-26 23:26:09 +0300

    final

--- PrivateChat/chatRoomClientRealtime.py
+++ PrivateChat/chatRoomClientRealtime.py
@@ -1,7 +1,19 @@
 import socket
 import errno
 import threading
-
+from os import system, name 
+import sys   
+# clear console
+def clearConsole(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+  
 class KeyboardThread(threading.Thread):
 
     def __init__(self, input_cbk = None, name='keyboard-input-thread'):
@@ -35,22 +47,75 @@ client_socket.setblocking(False)
 username = my_username.encode('utf-8')
 username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
 client_socket.send(username_header + username)
-# mesaj gönderme fonku
-def send_message(msg):
-    # Kullanıcıdan mesaj almayı bekle
-    message = msg
+# clientın yapabileceği işlemler
+def clientOps(keyboardInput):
+    condition=""
+    tmp=keyboardInput.split('>')
+    # eğer > ibaresi ile bir hedef gösterilmemişse tmp sadece bir string içerir
+    if len(tmp)>1:
+        condition=tmp[0]
+    #Durumlar
+    #1. Özel bir konuşma
+    if condition=="conversation":
+        #bu durumda tmp[1] konuşma yapılan kişiyi belirtir (conservation>name)
+        print("conversation",tmp[1])
+        #all chat
+        if tmp[1]=="all":
+            clearConsole()
+            print("Client: "+my_username)
+            filename    =   my_username+"&all.txt"
+            fileptr =open(filename,"r")
+            print(fileptr.readline())
+            lines=fileptr.readlines()
+            for line in lines:
+                print(line)
+            fileptr.close()
+        else:
+            clearConsole()
+            print("Client: "+my_username)
+            filename    =   my_username+"&"+tmp[1]+".txt"
+            fileptr =open(filename,"r")            
+            lines=fileptr.readlines()
+            for line in lines:
+                print(line)
+            fileptr.close()
 
-    # Mesaj boş değilse yolla
-    if message:
+    #mesaj yollamak
+    else:
+        #
+        message = keyboardInput
+        # Mesaj boş değilse yolla
+        if message:
 
-        # Mesajı göndermek için byte haline getir ve yolla
-        message = message.encode('utf-8')
-        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-        client_socket.send(message_header + message)
+            # Mesajı göndermek için byte haline getir ve yolla
+            message = message.encode('utf-8')
+            message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+            client_socket.send(message_header + message)
+            #Yollanan mesajı kaydet
+            #mesajı bytedan stringe geri çevir            
+            message = message.decode('utf-8')
+            target=""            
+            tmp=message.split('>')
+            # eğer > ibaresi ile bir hedef gösterilmemişse tmp sadece bir string içerir
+            if len(tmp)>1:
+                target=tmp[0]
+            #konuşmayı client tarafında sakla
+            #özel mesaj ise ayrı bir dosyada sakla
+            if target!="":
+                filename    =   my_username+"&"+target+".txt"
+                fileptr =open(filename,"a+")
+                fileptr.write(my_username+">"+target+">"+message)
+                fileptr.close()
+            #herkese açık mesajları sakla
+            else:
+                filename    =   my_username+"&all.txt"
+                fileptr =open(filename,"a+")
+                fileptr.write(my_username+">"+message)
+                fileptr.close()
 
 
 #start the Keyboard thread
-kthread = KeyboardThread(send_message)
+kthread = KeyboardThread(clientOps)
 while True:
     
     
@@ -59,7 +124,7 @@ while True:
         # Diğer kullanıcıları servera yolladığı mesajları alma
         while True:
 
-            # Receive our "header" containing username length, it's size is defined and constant
+            #
             username_header = client_socket.recv(HEADER_LENGTH)
 
             # Eğer mesaj alınmıyor ise server kapanmıştır
@@ -77,7 +142,26 @@ while True:
             message_header = client_socket.recv(HEADER_LENGTH)
             message_length = int(message_header.decode('utf-8').strip())
             message = client_socket.recv(message_length).decode('utf-8')
-
+            #Mesaj clienta özelmi gönderildi?
+            target=""            
+            tmp=message.split('>')
+            # eğer > ibaresi ile bir hedef gösterilmemişse tmp sadece bir string içerir
+            if len(tmp)>1:
+                target=tmp[0]
+            #konuşmayı client tarafında sakla
+            #özel mesaj ise ayrı bir dosyada sakla
+            if target!="":
+                filename    =   my_username+"&"+username+".txt"
+                fileptr =open(filename,"a+")
+                fileptr.write(username+">"+message+"\n")
+                fileptr.close()
+            #herkese açık mesajları sakla
+            else:
+                filename    =   my_username+"&all.txt"
+                fileptr =open(filename,"a+")
+                fileptr.write(username+">"+message+"\n")
+                fileptr.close()
+            #
             print(f'{username} > {message}')
 
     except IOError as e:
